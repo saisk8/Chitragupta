03ef24e7fd680a77e059d6d48e4575258cda3126
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-01-25 17:54:17 +0530

    Display cards; add color messages

--- js/index.js
+++ js/index.js
@@ -1,92 +1,17 @@
 /* eslint-disable no-undef */
-const CELL_SIZE = 14;
-const NUMBER_OF_COLORS = 6;
 
 window.onload = () => {
-  function createHeatMap(data, startYear) {
-    const width = 900;
-    const height = 110;
-    const dx = 35;
-    const gridClass = 'js-date-grid day';
-    const formatColor = d3
-      .scaleQuantize()
-      .domain([0, 10])
-      .range(d3.range(NUMBER_OF_COLORS).map(d => `color${d}`));
-
-    const heatmapSvg = d3
-      .select('.js-heatmap')
-      .selectAll('svg.heatmap')
-      .enter()
-      .append('svg')
-      .data(d3.range(startYear))
-      .enter()
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('class', 'color');
-
-    // Add a grid for each day between the date range.
-    const rect = heatmapSvg.append('g').attr('transform', `translate(${dx},0)`);
-
-    // Add year label.
-    rect
-      .append('text')
-      .attr('transform', `translate(-9,${CELL_SIZE * 3.5})rotate(-90)`)
-      .style('text-anchor', 'middle')
-      .text(d => d);
-
-    rect
-      .selectAll('.day')
-      // The heatmap will contain all the days in that year.
-      .data(d => d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1)))
-      .enter()
-      .append('rect')
-      .attr('class', gridClass)
-      .attr('width', CELL_SIZE)
-      .attr('height', CELL_SIZE)
-      .attr('x', d => d3.timeFormat('%U')(d) * CELL_SIZE)
-      .attr('y', d => d.getDay() * CELL_SIZE)
-      .attr('data-toggle', 'tooltip')
-      .datum(d3.timeFormat('%Y-%m-%d'))
-      // Add the grid data as a title attribute to render as a tooltip.
-      .attr('title', d => {
-        const countData = data.dates[d];
-        const date = d3.timeFormat('%b %d, %Y')(new Date(d));
-        if (!countData || !countData.count) return `Nothing`;
-        if (countData.count === 1) return `1 post on ${date}`;
-        return `${countData.count} posts on ${date}`;
-      })
-      .attr('date', d => d)
-      // Add bootstrap's tooltip event listener.
-      .call(() =>
-        $('[data-toggle="tooltip"]').tooltip({
-          container: 'body',
-          placement: 'top',
-          position: { my: 'top' }
-        })
-      )
-      // Add the colors to the grids.
-      .filter(d => dates.indexOf(d) > -1)
-      .attr('class', d => `${gridClass} ${formatColor(data.dates[d].count)}`);
-
-    // Render x axis to show months
-    d3.select('.js-months')
-      .selectAll('svg.months')
-      .enter()
-      .append('svg')
-      .data([1])
-      .enter()
-      .append('svg')
-      .attr('width', 800)
-      .attr('height', 20)
-      .append('g')
-      .attr('transform', 'translate(0,10)')
-      .selectAll('.month')
-      .data(() => d3.range(12))
-      .enter()
-      .append('text')
-      .attr('x', d => d * (4.5 * CELL_SIZE) + dx)
-      .text(d => d3.timeFormat('%b')(new Date(0, d + 1, 0)));
+  function createCard(data) {
+    const date = new Date(data.date);
+    return `<div class="card border-danger mb-3 w-100">
+              <div class="card-header">
+                Date: ${date}
+              </div>
+              <div class="card-body">
+                <p class='text-danger'> Number of sm: ${data.sms}
+                <p class='text-warning'> Number of mas: ${data.mms}
+              </div>
+            </div>`;
   }
 
   let mCount = 0;
@@ -129,7 +54,9 @@ window.onload = () => {
       const sms = document.getElementById('svalue').innerHTML;
       const mms = document.getElementById('mvalue').innerHTML;
       if (sms === 'S' || mms === 'M') {
-        document.getElementById('err').innerHTML = 'You did not do anything';
+        document.getElementById(
+          'err'
+        ).innerHTML = `<h5 class='text-info'>You did not do anything -_-</h5>`;
         return;
       }
       axios
@@ -140,7 +67,12 @@ window.onload = () => {
           mms
         })
         .then(response => {
-          document.getElementById('err').innerHTML = JSON.stringify(response.data);
+          if (response.data === 'pass')
+            document.getElementById('err').innerHTML = `<h5 class='text-success'>Log stored</h5>`;
+          else
+            document.getElementById(
+              'err'
+            ).innerHTML = `<h5 class='text-danger'>An error occured, try again</h5>`;
         })
         .catch(error => {
           // eslint-disable-next-line no-console
@@ -152,11 +84,26 @@ window.onload = () => {
   document.getElementById('data').addEventListener(
     'click',
     () => {
-      window.location.href = '/data';
+      axios
+        .get('/data')
+        .then(response => {
+          console.log(response.data);
+          let content = '';
+          if (response.data.length) {
+            console.log('Here');
+            response.data.forEach(d => {
+              content += createCard(d);
+            });
+          } else {
+            content = `<h3 class='text-success'>Nice going, keep it up!</h3>`;
+          }
+          document.getElementById('results').innerHTML = content;
+        })
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
     },
     false
   );
-  // const yearFormat = d3.timeFormat('%Y');
-  // const year = yearFormat(new Date());
-  // createHeatMap(data, year);
 };
